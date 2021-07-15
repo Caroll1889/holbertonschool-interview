f21766d23baeed433bc9b4de4ce2a80d9b6a3ec4
Caroll1889 <diahancaroll@hotmail.com>
2021-07-14 20:56:37 -0500

    Island Perimeter

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def island(grid):
+    land = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if j-1 < 0 or grid[i][j-1] == 0:
+                    land += 1
+                if j+1 >= len(grid[0]) or grid[i][j+1] == 0:
+                    land += 1
+                if i-1 < 0 or grid[i-1][j] == 0:
+                    land += 1
+                if i+1 >= len(grid) or grid[i+1][j] == 0:
+                    land += 1
+    return land
